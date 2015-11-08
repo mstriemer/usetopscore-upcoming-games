ff368f36e363fa099f7dada37b0cd57b282745c0
Mark Striemer <mstriemer@mozilla.com>
2015-11-08 17:52:31 -0600

    Refactor webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var plugins = [];
+var entry = [];
+if (process.env.DEV_SERVER) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    entry.push('webpack-dev-server/client?http://localhost:3000',
+               'webpack/hot/only-dev-server');
+}
+entry.push('./index');
+plugins.push(new webpack.NoErrorsPlugin());
+
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  resolve: {
-    extensions: ['', '.js']
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/
-    }]
-  }
+    devtool: 'eval',
+    entry: entry,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: plugins,
+    resolve: {
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel'],
+            exclude: /node_modules/
+        }]
+    }
 };
